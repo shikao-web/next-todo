de833f61069d68202c63be2bc267b0fa3bd864fb
shikaoweb <sikao1st30@gmail.com>
2026-03-30 13:28:23 +0900

    Made POST Logic

--- app/api/v1/post/route.ts
+++ app/api/v1/post/route.ts
@@ -1,4 +1,5 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { save_todo_to_DB } from '@/components/features/actions';
 
 export async function GET(req: Request, res: Response) {
     return Response.redirect(new URL('/', req.url));
@@ -7,6 +8,7 @@ export async function GET(req: Request, res: Response) {
 export async function POST(req: Request, res: Response) {
     const formData = await req.formData();
     const task: string = formData.get("task") as string;
-    console.log(task);  // これをデータベース保存処理に変える
+    console.log(task);
+    save_todo_to_DB(task);
     return Response.redirect(new URL('/', req.url));
 }
---
+++ lib/prisma.ts
@@ -0,0 +1,7 @@
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from "@prisma/client";
+
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
+
+export default prisma;
